d2b4f1c8e5965b89305cdfa2bf73db37f667fd46
nelsonrinconc86 <nelsonrinconc86@gmail.com>
2023-04-14 16:06:14 -0500

    v1

---
+++ Ejemplo1-MVC/CodigoMvC1.py
@@ -0,0 +1,10 @@
+import tkinter as Ventana
+
+class Vista():
+    def __init__(self):
+        self.Formulario=Ventana.Tk()
+        self.Formulario.title("formulario Registro Cliente")
+        
+        self.Formulario.mainloop()
+        
+Formulario=Vista()
