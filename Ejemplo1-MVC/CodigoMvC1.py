934a6d93311ab441239509a96facaf191be882e2
nelsonrinconc86 <nelsonrinconc86@gmail.com>
2023-04-14 17:08:02 -0500

    fallaError

--- Ejemplo1-MVC/CodigoMvC1.py
+++ Ejemplo1-MVC/CodigoMvC1.py
@@ -3,6 +3,8 @@ import tkinter as Ventana
 #********* Clase del Patron Vista.........*************************
 class Vista:
     def __init__(self):
+        print(" se ejecuta la clase Vista")
+        self.ObjCotroler=Controlador()
         self.miFormulario=Ventana.Tk()
         self.miFormulario.title("Formulario Registro Cliente")
         
@@ -19,9 +21,30 @@ class Vista:
     # Metodo que tomas los datos de los Entry para almacenarlos en Variables locales del metodo
     def TomarDatos(self):
         auxNombre=self.EntryNombre.get()
+        self.ObjCotroler.GuardarDatos(auxNombre)
+        
+        
         
     def InicioVentanas(self):
        self.miFormulario.mainloop()
+        
+#********* Clase del Patron Controlador.........*************************        
+class Controlador:
+    def __init__(self):
+        print(" se ejecuta la clase controladora")
+        self.ObjControlador=None
+        self.ObjVista=Vista()
+        
+    
+    def GuardarDatos(self):
+        self.ObjVista.TomarDatos()
+        print("se tomaron los datos")
+        
+
+        
+        
+        
+        
         
         
         
@@ -31,6 +54,7 @@ class CodigoPrincipal:
     def __init__(self):
         ObjVista=Vista()
         ObjVista.InicioVentanas()
+        objCotrolador=Controlador()
         
         
 
